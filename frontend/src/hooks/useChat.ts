2f0f8d7cfcdfc4e5effdcf3010ec95d7248f432f
Gaurav Bhatt <gauravkapadibhatt@gmail.com>
2026-07-10 09:15:44 +0545

    Made the UI changes for the newer groq models

--- frontend/src/hooks/useChat.ts
+++ frontend/src/hooks/useChat.ts
@@ -10,13 +10,10 @@ export function useChat() {
   const qc = useQueryClient()
   
   return useMutation({
-    mutationFn: (messages: Message[]) => 
-      axios.post('http://localhost:8000/chat', { messages }).then(r => r.data),
+    mutationFn: ({ messages, model }: { messages: Message[], model: string }) => 
+      axios.post('http://localhost:8000/chat', { messages, model }).then(r => r.data),
     onSuccess: (data, variables) => {
-      // qc.setQueryData(['conversation'], (prev: any) => [...(prev || []), ...variables, data])
-      // the problem was thay variables contained the entire message array (including previous messages), so spreading it suplicates the conversation history. 
-      qc.setQueryData(['conversation'], (prev: any) => [...(prev || []), variables[variables.length - 1], data])
-      // qc.setQueryData(['conversation'], (prev: any) => [...(prev || []), variables[variables.length - 1], data])
+      qc.setQueryData(['conversation'], (prev: any) => [...(prev || []), variables.messages[variables.messages.length - 1], data])
     }
   })
 }
